29d88cea8f8e6f51e02f3de5420e049798d68038
Zac Tawfick <101304267+ZikoGotGame@users.noreply.github.com>
2025-11-17 01:56:33 -0500

    feat: update website and switch emauil service.

--- script.js
+++ script.js
@@ -1,43 +1,48 @@
 var tablinks = document.getElementsByClassName("tab-links");
-    var tabcontents = document.getElementsByClassName("tab-contents");
+var tabcontents = document.getElementsByClassName("tab-contents");
 
-    function opentab(tabname){
-      for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-      }
-      for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-      }
-      event.currentTarget.classList.add("active-link");
-      document.getElementById(tabname).classList.add("active-tab");
-    }
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
 
 var sidemeu = document.getElementById("sidemenu");
 
-function openmenu(){
+function openmenu() {
   sidemeu.style.right = "0";
 }
 
-function closemenu(){
+function closemenu() {
   sidemeu.style.right = "-200px";
 }
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzUA6vDaPogxJVhL6NnBr2g2kl_q2Er8tsak9wSG6HWHNFR3TBrrKTmwGHoqUf2GcEh/exec'
-const msg = document.getElementById("msg")
+emailjs.init("Yqb1MjoCegfkNUzRP");
 
-var form = document.getElementById('sheetdb-form');
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  fetch(form.action, {
-      method : "POST",
-      body: new FormData(document.getElementById("sheetdb-form")),
-  }).then(
-      response => response.json()
-  ).then((html) => {
-    msg.innerHTML = "Your message has been sent"
-    setTimeout(function(){
-      msg.innerHTML = ""
-    },5000)
-    form.reset()
+document
+  .getElementById("sheetdb-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const form = event.target;
+
+    emailjs
+      .send("service_omfag1g", "template_fu7bwsx", {
+        from_name: form.elements["from_name"].value,
+        reply_to: form.elements["reply_to"].value,
+        message: form.elements["message"].value,
+      })
+      .then(() => {
+        msg.innerText = "Your message has been sent!";
+        setTimeout(() => (msg.innerText = ""), 5000);
+        form.reset();
+      })
+      .catch((error) => {
+        msg.innerText = "Failed to send message.";
+      });
   });
-});
